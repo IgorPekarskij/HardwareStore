fee812b98b98166657bbabf2d785d1654493347f
Igor <igorpekarskij.brest@gmail.com>
2016-05-23 15:21:14 +0300

    Create Vip customer

---
+++ src/Customers/VipCustomers.java
@@ -0,0 +1,31 @@
+package Customers;
+
+public class VipCustomers {
+    private String name;
+    private int discount;
+
+    public VipCustomers(String name, int discount) {
+        this.name = name;
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public void buyProductWithDiscount(String product, int discount, int countity) {
+
+    }
+}
