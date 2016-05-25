555bf0eefb12b3cd26a06c3b6ca3a5016d9e1818
Igor <igorpekarskij.brest@gmail.com>
2016-05-25 23:13:55 +0300

    Create bank

---
+++ src/StoreSection/Bank.java
@@ -0,0 +1,36 @@
+package StoreSection;
+
+public class Bank {
+    private String name;
+    private String creditTerms;
+
+    public Bank(String name, String creditTerms) {
+        this.name = name;
+        this.creditTerms = creditTerms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreditTerms() {
+        return creditTerms;
+    }
+
+    public void setCreditTerms(String creditTerms) {
+        this.creditTerms = creditTerms;
+    }
+
+    public void giveCredit(int sum){
+
+    }
+
+    public void checkCreditStory(String customer){
+
+    }
+
+}
