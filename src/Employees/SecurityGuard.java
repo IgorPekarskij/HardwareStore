f1959852ad5abd2dc4648bbb65f5770e73108b60
Igor <igorpekarskij.brest@gmail.com>
2016-05-25 23:02:34 +0300

    Create security guard

---
+++ src/Employees/SecurityGuard.java
@@ -0,0 +1,29 @@
+package Employees;
+
+public class SecurityGuard {
+    private String name;
+
+    public SecurityGuard(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void openDoor(){
+
+    }
+
+    public void closeDoor(){
+
+    }
+
+    public void checkCustomer(String customer){
+
+    }
+}
