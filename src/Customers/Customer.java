98038b64fd51d6fe16776351a32a16e10536fe61
Igor <igorpekarskij.brest@gmail.com>
2016-05-25 22:28:13 +0300

    Create customer

---
+++ src/Customers/Customer.java
@@ -0,0 +1,26 @@
+package Customers;
+
+public class Customer {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void buyProduct(String product, int countity) {
+
+    }
+
+    public void returnProduct(String product, int countity) {
+
+    }
+
+}
