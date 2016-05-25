c4caa1ed8a19faa988fc59bff735a5b65c4a58e6
Igor <igorpekarskij.brest@gmail.com>
2016-05-25 23:17:21 +0300

    Create money agent

---
+++ src/StoreSection/MoneyAgent.java
@@ -0,0 +1,41 @@
+package StoreSection;
+
+public class MoneyAgent {
+    private String name;
+    private double expirience;
+    private String bank;
+
+    public MoneyAgent(String name, double expirience, String bank) {
+        this.name = name;
+        this.expirience = expirience;
+        this.bank = bank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getExpirience() {
+        return expirience;
+    }
+
+    public void setExpirience(double expirience) {
+        this.expirience = expirience;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public void sendRequest(){
+
+    }
+}
