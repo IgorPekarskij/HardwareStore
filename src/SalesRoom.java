8aa4abc418aba413355703015dbfb92028743d83
Igor <igorpekarskij.brest@gmail.com>
2016-05-27 22:16:33 +0300

    Create super class sales room

--- src/SalesRoom.java
+++ src/SalesRoom.java
@@ -1,5 +1,16 @@
 public class SalesRoom {
     private String name;
-    private String consultante;
+    private Consultant consultante;
 
+    public SalesRoom(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
