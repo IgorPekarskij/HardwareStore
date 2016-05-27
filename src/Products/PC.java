d487b863acb4618ed6721ca9dfa6a3d66cc32a83
Igor <igorpekarskij.brest@gmail.com>
2016-05-27 21:42:42 +0300

    Extends from Product class

--- src/Products/PC.java
+++ src/Products/PC.java
@@ -1,46 +1,20 @@
 package Products;
 
-public class PC {
-    private String name;
-    private int amountOfRAM;
-    private String department;
+public class PC extends Product {
 
-    public PC(String name, int amountOfRAM, String department) {
-        this.name = name;
-        this.amountOfRAM = amountOfRAM;
-        this.department = department;
+    public PC(String name, String department, int ram) {
+        super(name, department, ram);
     }
 
-    public String getName() {
-        return name;
+
+    @Override
+    public void turnOn(){
+        super.turnOn();
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAmountOfRAM() {
-        return amountOfRAM;
-    }
-
-    public void setAmountOfRAM(int amountOfRAM) {
-        this.amountOfRAM = amountOfRAM;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public void turnOnPC(){
-
-    }
-
-    public void turnOfPC(){
-
+    @Override
+    public void turnOff(){
+        super.turnOff();
     }
 
     public void loadOperationSystem(String operationSystem) {
