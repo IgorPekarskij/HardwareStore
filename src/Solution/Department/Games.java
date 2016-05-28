062de7a3ad01e0194088b8da14f7da3a35b03314
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 18:05:16 +0300

    Refactor code with using inheritance and composition

--- src/Solution/Department/Games.java
+++ src/Solution/Department/Games.java
@@ -1,17 +1,9 @@
 package Solution.Department;
 
-import Solution.Consultant;
-import Solution.Products.Product;
-import Solution.SalesRoom;
+public class Games extends BaseDepartment {
 
-public class Games extends Department {
-
-
-    public Games(String name, Product products, SalesRoom salesRoom, Consultant consultant) {
-        super(name, products, salesRoom, consultant);
+    public Games(String name) {
+        super(name);
     }
 
-    public Games(String name, Product products) {
-        super(name, products);
-    }
 }
