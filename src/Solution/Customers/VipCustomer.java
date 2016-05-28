e4951783f0b520bef3c4641e564f04e56d3e5538
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 17:27:11 +0300

    Refactor code with using inheritance and composition

--- src/Solution/Customers/VipCustomer.java
+++ src/Solution/Customers/VipCustomer.java
@@ -5,8 +5,8 @@ import Solution.SalesRoom;
 public class VipCustomer extends BaseCustomer {
     private int discount;
 
-    public VipCustomer(String name, SalesRoom salesRoom, int discount) {
-        super(name, salesRoom);
+    public VipCustomer(String name, int discount) {
+        super(name);
         this.discount = discount;
     }
 
@@ -18,12 +18,16 @@ public class VipCustomer extends BaseCustomer {
         this.discount = discount;
     }
 
-    public void buyProductWithDiscount() {
-
+    @Override
+    public void buyProduct() {
+        if (!chekDiscount()){
+            buyProduct();
+        } else {
+            // купить со скидкой
+        }
     }
 
-    @Override
-    public void returnProduct() {
-        super.returnProduct();
+    boolean chekDiscount() {
+        return discount > 0;
     }
 }
