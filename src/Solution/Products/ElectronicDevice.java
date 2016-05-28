ca3c630f911ae6c51b4dfbea8f5b2c9689bce6f2
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 23:21:08 +0300

    Refactor code with using interface

---
+++ src/Solution/Products/ElectronicDevice.java
@@ -0,0 +1,16 @@
+package Solution.Products;
+
+import Solution.Interfaces.ElectronicDeviceInterface;
+
+public class ElectronicDevice extends BaseProduct implements ElectronicDeviceInterface {
+
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
