e91e57d9c4ae88989dba3dee2a970e837d4c7abb
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 17:05:05 +0300

    Create common customer

---
+++ src/Solution/Customers/CommonCasrtomer.java
@@ -0,0 +1,13 @@
+package Solution.Customers;
+
+import Solution.SalesRoom;
+
+/**
+ * Created by Игорь on 28.05.2016.
+ */
+public class CommonCasrtomer extends BaseCustomer {
+
+    public CommonCasrtomer(String name, SalesRoom salesRoom) {
+        super(name, salesRoom);
+    }
+}
