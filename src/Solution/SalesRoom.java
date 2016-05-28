267b9bd0189a0b845b1eee975d0cd7ed50c33794
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 18:04:37 +0300

    Refactor code with using inheritance and composition

--- src/Solution/SalesRoom.java
+++ src/Solution/SalesRoom.java
@@ -1,34 +1,18 @@
 package Solution;
 
-import Solution.Bank.Bank;
-import Solution.Consultant;
-import Solution.Customers.Customer;
-import Solution.Department.Department;
-import Solution.Employees.Cashier;
-import Solution.Employees.Employee;
+import Solution.Customers.BaseCustomer;
+import Solution.Department.BaseDepartment;
+import Solution.Employees.Administrator;
+
+import java.util.ArrayList;
 
 
 public class SalesRoom {
     private String name;
-    private Consultant consultante;
-    private Customer customer;
-    private Bank bank;
-    private Department department;
+    private ArrayList<BaseCustomer> baseCustomer;
+    private ArrayList<BaseDepartment> baseDepartment;
+    private Administrator administrator;
 
-    public SalesRoom(String name, Consultant consultante, Customer customer, Bank bank, Department department) {
-        this.name = name;
-        this.consultante = consultante;
-        this.customer = customer;
-        this.bank = bank;
-        this.department = department;
-    }
-
-    public SalesRoom(String name, Consultant consultante, Customer customer, Department department) {
-        this.name = name;
-        this.consultante = consultante;
-        this.customer = customer;
-        this.department = department;
-    }
 
     public String getName() {
         return name;
