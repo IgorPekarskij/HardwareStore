ae324483d36516f76bf2ed460d069f5d079bb686
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 22:02:56 +0300

    Correct issues code

--- src/Solution/Customers/CommonCasrtomer.java
+++ src/Solution/Customers/CommonCasrtomer.java
@@ -1,10 +1,7 @@
 package Solution.Customers;
 
-import Solution.SalesRoom;
 
 public class CommonCasrtomer extends BaseCustomer {
 
-    public CommonCasrtomer(String name) {
-        super(name);
-    }
+
 }
