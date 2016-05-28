8fdd6ae1d36c917e95f47684816398acb85dd8f9
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 23:18:43 +0300

    Refactor code with using interfaces

--- src/Solution/Employees/SecurityGuard.java
+++ src/Solution/Employees/SecurityGuard.java
@@ -1,19 +1,9 @@
 package Solution.Employees;
 
 import Solution.Customers.BaseCustomer;
-import Solution.SalesRoom;
 
 public class SecurityGuard extends BaseEmployee {
-    private BaseCustomer baseCustomer;
 
-    public SecurityGuard(String name, BaseCustomer baseCustomer) {
-        super(name);
-        this.baseCustomer = baseCustomer;
-    }
-
-    public BaseCustomer getBaseCustomer() {
-        return baseCustomer;
-    }
 
     public void openDoor(){
 
