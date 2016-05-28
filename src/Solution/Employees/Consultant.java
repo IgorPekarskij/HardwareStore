0b7592a443445e0bc82d4bcba67ccbd5aa0d5d73
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 23:18:33 +0300

    Refactor code with using interfaces

--- src/Solution/Employees/Consultant.java
+++ src/Solution/Employees/Consultant.java
@@ -3,20 +3,6 @@ package Solution.Employees;
 import Solution.Department.BaseDepartment;
 
 public class Consultant extends BaseEmployee {
-    private BaseDepartment baseDepartment;
-
-    public Consultant(String name, boolean busyness, BaseDepartment baseDepartment) {
-        super(name, busyness);
-        this.baseDepartment = baseDepartment;
-    }
-
-    public BaseDepartment getBaseDepartment() {
-        return baseDepartment;
-    }
-
-    public void setBaseDepartment(BaseDepartment baseDepartment) {
-        this.baseDepartment = baseDepartment;
-    }
 
     public void helpCustomer(){
 
