9987c7d243cd7986dd5baee703d74cf502a202ca
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 16:26:19 +0300

    Refactor code with using inheritance and composition

--- src/Solution/Employees/Employee.java
+++ src/Solution/Employees/Employee.java
@@ -1,16 +1,31 @@
 package Solution.Employees;
 
+import Solution.SalesRoom;
+
 public class Employee {
     private String name;
     private boolean busyness;
+    private SalesRoom salesRoom;
 
-    public Employee(String name) {
+    public Employee(String name, SalesRoom salesRoom) {
         this.name = name;
+        this.salesRoom = salesRoom;
     }
 
     public Employee(String name, boolean busyness) {
         this.name = name;
         this.busyness = busyness;
+    }
+
+    public SalesRoom getSalesRoom() {
+        return salesRoom;
+    }
+
+    public Employee(String name, boolean busyness, SalesRoom salesRoom) {
+
+        this.name = name;
+        this.busyness = busyness;
+        this.salesRoom = salesRoom;
     }
 
     public String getName() {
