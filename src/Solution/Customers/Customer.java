e7b6cb062dcaf53faaa3d13faa4326141f83d090
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 15:34:01 +0300

    Refactor code with using inheritance and composition

--- src/Solution/Customers/Customer.java
+++ src/Solution/Customers/Customer.java
@@ -1,10 +1,14 @@
-package Customers;
+package Solution.Customers;
+
+import Solution.SalesRoom;
 
 public class Customer {
     private String name;
+    private SalesRoom salesRoom;
 
-    public Customer(String name) {
+    public Customer(String name, SalesRoom salesRoom) {
         this.name = name;
+        this.salesRoom = salesRoom;
     }
 
     public String getName() {
