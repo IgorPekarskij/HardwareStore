cdec013fbf3bcb5b6874a506cc586c34ddd0c1a1
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 22:32:18 +0300

    Refactor code with using interface

--- src/Solution/Department/Computers.java
+++ src/Solution/Department/Computers.java
@@ -1,11 +1,6 @@
 package Solution.Department;
 
-import Solution.Products.BaseProduct;
-import Solution.SalesRoom;
-
 public class Computers extends BaseDepartment {
 
-    public Computers(String name, BaseProduct products, SalesRoom salesRoom) {
-        super(name);
-    }
+
 }
