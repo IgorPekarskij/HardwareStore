c2c47e4a5464016881fc06103f4f73f297c607f6
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 17:49:27 +0300

    Refactor code with using inheritance and composition

--- src/Solution/Products/TV.java
+++ src/Solution/Products/TV.java
@@ -2,15 +2,11 @@ package Solution.Products;
 
 import Solution.Department.HomeElectronics;
 
-public class TV extends Product{
+public class TV extends BaseProduct {
     private String model;
     private HomeElectronics homeElectronics;
 
-    public TV(String name, HomeElectronics homeElectronics, String model) {
-        super(name);
-        this.model = model;
-        this.homeElectronics = homeElectronics;
-    }
+
 
     public String getModel() {
         return model;
@@ -18,15 +14,6 @@ public class TV extends Product{
 
     public void setModel(String model) {
         this.model = model;
-    }
-
-    public void turnOn() {
-        super.turnOn();
-    }
-
-    @Override
-    public void turnOff() {
-        super.turnOff();
     }
 
     public void showChanel(int number) {
