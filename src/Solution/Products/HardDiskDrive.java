599f9eb4ccfecc306fcd22cb879f1614a0ed88b7
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 15:32:41 +0300

    Refactor code with using inheritance and composition

--- src/Solution/Products/HardDiskDrive.java
+++ src/Solution/Products/HardDiskDrive.java
@@ -1,11 +1,15 @@
-package Products;
+package Solution.Products;
+
+import Solution.Department.Computers;
 
 public class HardDiskDrive extends Product{
     private String amountOfMemmory;
+    private Computers computers;
 
-    public HardDiskDrive(String name, String department, String amountOfMemmory) {
-        super(name, department);
+    public HardDiskDrive(String name, Computers computers, String amountOfMemmory) {
+        super(name);
         this.amountOfMemmory = amountOfMemmory;
+        this.computers = computers;
     }
 
     public String getAmountOfMemmory() {
