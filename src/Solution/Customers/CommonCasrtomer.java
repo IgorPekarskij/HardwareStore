968cf7f4436249b106948b58b55bb299ea79671f
Игорь <igorpekarskij.brest@gmail.com>
2016-05-28 17:27:05 +0300

    Refactor code with using inheritance and composition

--- src/Solution/Customers/CommonCasrtomer.java
+++ src/Solution/Customers/CommonCasrtomer.java
@@ -2,12 +2,9 @@ package Solution.Customers;
 
 import Solution.SalesRoom;
 
-/**
- * Created by Игорь on 28.05.2016.
- */
 public class CommonCasrtomer extends BaseCustomer {
 
-    public CommonCasrtomer(String name, SalesRoom salesRoom) {
-        super(name, salesRoom);
+    public CommonCasrtomer(String name) {
+        super(name);
     }
 }
