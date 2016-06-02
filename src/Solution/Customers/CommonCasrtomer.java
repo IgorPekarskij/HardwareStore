cdad9060b0cc985213ae281a506da9885e173b3b
Igor <igorpekarskij.brest@gmail.com>
2016-06-03 00:07:42 +0300

    Add constructor Partly implements method buyProduct

--- src/Solution/Customers/CommonCasrtomer.java
+++ src/Solution/Customers/CommonCasrtomer.java
@@ -1,7 +1,33 @@
 package Solution.Customers;
 
 
-public class CommonCasrtomer extends BaseCustomer {
+import Solution.Interfaces.EmployeeInterface;
+import Solution.Interfaces.ProductInterface;
 
+public class CommonCasrtomer extends AbstractCustomer {
+
+    public CommonCasrtomer(String name, int cash, String history) {
+        super(name, cash, history);
+    }
+
+    @Override
+    public String buyProduct(ProductInterface product, int quantity) {
+        EmployeeInterface banker;
+        if(product.getPrice() < getCash() && quantity < product.getQuantityOfProduct()){
+            return "Thank you for your purchase";
+        }
+        else if(product.getPrice() < getCash() && quantity > product.getQuantityOfProduct()){
+            return "Sorry do not have the right amount of product";
+        }
+        else if(product.getPrice() > getCash() && quantity < product.getQuantityOfProduct()) {
+            return null;    // Нужно реализовать покупку через кредит
+        }
+        return null;
+    }
+
+    @Override
+    public int returnProduct(ProductInterface product, int quantity) {
+        return 0;
+    }
 
 }
