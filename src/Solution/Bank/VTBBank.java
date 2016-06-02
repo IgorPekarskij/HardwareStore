b780e209d969cbf084c35be35db3d8df490a5694
Igor <igorpekarskij.brest@gmail.com>
2016-06-02 23:12:01 +0300

    Add constructor Implements method getCredit

--- src/Solution/Bank/VTBBank.java
+++ src/Solution/Bank/VTBBank.java
@@ -1,5 +1,20 @@
 package Solution.Bank;
 
 
-public class VTBBank extends BaseBank {
+import Solution.Interfaces.CustomerInterface;
+import Solution.Interfaces.ProductInterface;
+
+public class VTBBank extends AbstractBank {
+
+    public VTBBank(String name, String creditDescription) {
+        super(name, creditDescription);
+    }
+
+    @Override
+    public int giveCredit(int amount, CustomerInterface customer, ProductInterface product) {
+        if(!checkInfo(customer.getHistory())){
+            return amount;
+        }
+        return 0;
+    }
 }
