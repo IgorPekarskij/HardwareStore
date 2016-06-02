0fc37fdf5a1fc14e15e69970b960cf1256147e76
Igor <igorpekarskij.brest@gmail.com>
2016-06-02 23:11:21 +0300

    Implements method getCredit

--- src/Solution/Bank/AlfaBank.java
+++ src/Solution/Bank/AlfaBank.java
@@ -1,5 +1,22 @@
 package Solution.Bank;
 
 
-public class AlfaBank extends BaseBank {
+import Solution.Interfaces.CustomerInterface;
+import Solution.Interfaces.ProductInterface;
+
+public class AlfaBank extends AbstractBank {
+
+    public AlfaBank(String name, String creditDescription) {
+        super(name, creditDescription);
+    }
+
+    @Override
+    public int giveCredit(int amount, CustomerInterface customer, ProductInterface product) {
+        if(product.getPrice() > customer.getCash()) {
+            if (checkInfo(customer.getHistory())){
+                return (product.getPrice() - customer.getCash());
+            }
+        }
+        return 0;
+    }
 }
