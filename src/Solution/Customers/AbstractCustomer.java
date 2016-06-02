26f5291e098a95de06b2e72ece362e0ecd9a1d08
Igor <igorpekarskij.brest@gmail.com>
2016-06-03 00:06:38 +0300

    Add field history and cash Add constructor Add getters and setters

--- src/Solution/Customers/AbstractCustomer.java
+++ src/Solution/Customers/AbstractCustomer.java
@@ -1,30 +1,39 @@
 package Solution.Customers;
 
 import Solution.Interfaces.CustomerInterface;
+import Solution.Interfaces.ProductInterface;
 
 public abstract class AbstractCustomer implements CustomerInterface{
     private String name;
     private int cash;
+    private String history;
 
-    public AbstractCustomer(String name, int cash) {
+    public AbstractCustomer(String name, int cash, String history) {
         this.name = name;
         this.cash = cash;
+        this.history = history;
     }
 
     public String getName() {
         return name;
     }
 
-    public void buyProduct() {
+    @Override
+    abstract public String buyProduct(ProductInterface product, int quantity);
 
-    }
-
-    public void returnProduct() {
-
+    @Override
+    public String getHistory() {
+        return history;
     }
 
     @Override
     public int getCash() {
         return cash;
     }
+
+    @Override
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
+
 }
