34efb37d55c0e27682165b6171283cf3afc48126
Igor <igorpekarskij.brest@gmail.com>
2016-06-03 00:15:10 +0300

    Add

--- src/Solution/Products/ElectronicDevice.java
+++ src/Solution/Products/ElectronicDevice.java
@@ -2,7 +2,7 @@ package Solution.Products;
 
 import Solution.Interfaces.ElectronicDeviceInterface;
 
-public class ElectronicDevice extends BaseProduct implements ElectronicDeviceInterface {
+public class ElectronicDevice extends AbstractProduct implements ElectronicDeviceInterface {
 
     @Override
     public void turnOn() {
